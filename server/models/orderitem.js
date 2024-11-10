1525cee2637116b36755c00f328b87472d80a3e4
Fenil-Mansukhbhai-Vaghasiya <vaghasiya.f@northeastern.edu>
2024-11-10 17:18:30 -0500

    added models and created migrations files

---
+++ server/models/orderitem.js
@@ -0,0 +1,30 @@
+module.exports = (sequelize, DataTypes) => {
+    const OrderItem = sequelize.define(
+      "OrderItem",
+      {
+        id: {
+          type: DataTypes.UUID,
+          primaryKey: true,
+          defaultValue: DataTypes.UUIDV4,
+        },
+        uOrderId: {
+          type: DataTypes.UUID,
+          allowNull: false,
+        },
+        uProductItemId: {
+          type: DataTypes.UUID,
+          allowNull: false,
+        },
+        iQuantity: {
+          type: DataTypes.INTEGER,
+        },
+      },
+      {
+        tableName: "OrderItem",
+        freezeTableName: "OrderItem",
+      }
+    );
+  
+    return OrderItem;
+  };
+  
