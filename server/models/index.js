a765ba255ec6599f54698d1115cbe846b3cf6497
Fenil-Mansukhbhai-Vaghasiya <vaghasiya.f@northeastern.edu>
2024-11-10 22:46:44 -0500

    setup user controller

--- server/models/index.js
+++ server/models/index.js
@@ -56,4 +56,30 @@ Object.keys(db).forEach((modelName) => {
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
+db.User = require("./user")(sequelize, DataTypes);
+db.Address = require("./address")(sequelize, DataTypes);
+db.Brand = require("./brand")(sequelize, DataTypes);
+db.Cart = require("./cart")(sequelize, DataTypes);
+db.CartItem = require("./cartitem")(sequelize, DataTypes);
+db.Category = require("./category")(sequelize, DataTypes);
+db.Color = require("./color")(sequelize, DataTypes);
+db.DiscountCode = require("./discountcode")(sequelize, DataTypes);
+db.Group = require("./group")(sequelize, DataTypes);
+db.Order = require("./order")(sequelize, DataTypes);
+db.OrderItem = require("./orderitem")(sequelize, DataTypes);
+db.Product = require("./product")(sequelize, DataTypes);
+db.ProductImage = require("./productimage")(sequelize, DataTypes);
+db.ProductItem = require("./productitem")(sequelize, DataTypes);
+db.ProductSpecification = require("./productspecification")(
+  sequelize,
+  DataTypes
+);
+db.ProductVariation = require("./productvariation")(sequelize, DataTypes);
+db.Review = require("./review")(sequelize, DataTypes);
+db.ShippingType = require("./shippingtype")(sequelize, DataTypes);
+db.SizeCategory = require("./sizecategory")(sequelize, DataTypes);
+db.SizeOption = require("./sizeoption")(sequelize, DataTypes);
+db.SpecificationOption = require("./specificationoption")(sequelize, DataTypes);
+db.SubCategory = require("./subcategory")(sequelize, DataTypes);
+
 module.exports = db;
