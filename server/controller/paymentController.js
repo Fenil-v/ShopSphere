b9f9ad0e1681c26f075a4310cbe3dae4582049cd
fenil <fenil12vaghasiya@gmail.com>
2024-11-17 15:33:15 -0500

    updated payment controller

--- server/controller/paymentController.js
+++ server/controller/paymentController.js
@@ -1,11 +1,10 @@
 const stripe = require("stripe")(
-  "sk_test_51OgiXySF7soF3JJNGxVTo2KG7bErELfjBlN3cWnWrqj8JwgsHOLLymn9vtooYBCUufEg3fgw941He2kE4B0wksmW00y45ZvNux"
+  process.env.STRIP_KEY
 );
 
-exports.createPaymentIntent = async (req, res, next) => {
+export async function createPaymentIntent(req, res, next) {
   try {
     // Create a PaymentIntent with the order amount and currency
-    console.log(req.body, "*****************amount");
     const paymentIntent = await stripe.paymentIntents.create({
       amount: req.body.amount,
       currency: "INR",
@@ -23,4 +22,4 @@ exports.createPaymentIntent = async (req, res, next) => {
       error: "Internal Server Error",
     });
   }
-};
+}
